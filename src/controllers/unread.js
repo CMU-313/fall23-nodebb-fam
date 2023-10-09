af4396b7c809686519102fce5c935f44902cb6db
mingminggl <88123825+mingminggl@users.noreply.github.com>
2023-10-08 23:09:25 -0400

    added routes for resolve

--- src/controllers/unread.js
+++ src/controllers/unread.js
@@ -11,6 +11,7 @@ const topics = require('../topics');
 const helpers = require('./helpers');
 
 const unreadController = module.exports;
+const unresolvedController = module.exports;
 const relative_path = nconf.get('relative_path');
 
 unreadController.get = async function (req, res) {
@@ -72,6 +73,70 @@ unreadController.unreadTotal = async function (req, res, next) {
     try {
         const unreadCount = await topics.getTotalUnread(req.uid, filter);
         res.json(unreadCount);
+    } catch (err) {
+        next(err);
+    }
+};
+
+unresolvedController.get = async function (req, res) {
+    const { cid } = req.query;
+    const filter = req.query.filter || '';
+
+    const [categoryData, userSettings, isPrivileged] = await Promise.all([
+        helpers.getSelectedCategory(cid),
+        user.getSettings(req.uid),
+        user.isPrivileged(req.uid),
+    ]);
+
+    const page = parseInt(req.query.page, 10) || 1;
+    const start = Math.max(0, (page - 1) * userSettings.topicsPerPage);
+    const stop = start + userSettings.topicsPerPage - 1;
+    const data = await topics.getUnresolvedTopics({
+        cid: cid,
+        uid: req.uid,
+        start: start,
+        stop: stop,
+        filter: filter,
+        query: req.query,
+    });
+
+    const isDisplayedAsHome = !(req.originalUrl.startsWith(`${relative_path}/api/unresolved`) || req.originalUrl.startsWith(`${relative_path}/unresolved`));
+    const baseUrl = isDisplayedAsHome ? '' : 'unresolved';
+
+    if (isDisplayedAsHome) {
+        data.title = meta.config.homePageTitle || '[[pages:home]]';
+    } else {
+        data.title = '[[pages:unresolved]]';
+        data.breadcrumbs = helpers.buildBreadcrumbs([{ text: '[[unresolved:title]]' }]);
+    }
+
+    data.pageCount = Math.max(1, Math.ceil(data.topicCount / userSettings.topicsPerPage));
+    data.pagination = pagination.create(page, data.pageCount, req.query);
+    helpers.addLinkTags({ url: 'unresolved', res: req.res, tags: data.pagination.rel });
+
+    if (userSettings.usePagination && (page < 1 || page > data.pageCount)) {
+        req.query.page = Math.max(1, Math.min(data.pageCount, page));
+        return helpers.redirect(res, `/unresolved?${querystring.stringify(req.query)}`);
+    }
+    data.showSelect = true;
+    data.showTopicTools = isPrivileged;
+    data.allCategoriesUrl = `${baseUrl}${helpers.buildQueryString(req.query, 'cid', '')}`;
+    data.selectedCategory = categoryData.selectedCategory;
+    data.selectedCids = categoryData.selectedCids;
+    data.selectCategoryLabel = '[[unresolved:mark_as_resolved]]';
+    data.selectCategoryIcon = 'fa-inbox';
+    data.showCategorySelectLabel = true;
+    data.filters = helpers.buildFilters(baseUrl, filter, req.query);
+    data.selectedFilter = data.filters.find(filter => filter && filter.selected);
+
+    res.render('unresolved', data);
+};
+
+unresolvedController.unreadTotal = async function (req, res, next) {
+    const filter = req.query.filter || '';
+    try {
+        const unresolvedCount = await topics.getTotalUnresolved(req.uid, filter);
+        res.json(unresolvedCount);
     } catch (err) {
         next(err);
     }
